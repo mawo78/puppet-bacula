2257723ad84107a646d3cc782ba67a3918e31fdd
Romain Tartière <romain@blogreen.org>
2017-05-19 12:58:00 +0200

    Generate PKI Cert + Key
    While the name PKI may lead you to think some Public Key Infrastructure
    is usable here, it is not true.
    
    The certificate in the bundle MUST be a self-signed Certification
    Authority certificate.

--- manifests/pki.pp
+++ manifests/pki.pp
@@ -8,24 +8,28 @@ class bacula::pki(
   $bacula_user  = $::bacula::bacula_user
   $bacula_group = $::bacula::bacula_group
 
-  $certfile = "${conf_dir}/ssl/${trusted['certname']}_cert.pem"
-  $keyfile  = "${conf_dir}/ssl/${trusted['certname']}_key.pem"
-  $cafile   = "${conf_dir}/ssl/ca.pem"
+  $certfile = "${conf_dir}/ssl/${trusted['certname']}_pki_cert.pem"
+  $keyfile  = "${conf_dir}/ssl/${trusted['certname']}_pki_key.pem"
 
   $pki_keypair  = "${conf_dir}/ssl/pki-keypair.pem"
+
+  exec { 'bacula_pki_key':
+    command => "/usr/bin/openssl genrsa -out ${keyfile} 4096",
+    creates => $keyfile,
+    require => File["${conf_dir}/ssl"]
+  }
+
+  exec { 'bacula_pki_cert':
+    command => "/usr/bin/openssl req -new -key ${keyfile} -x509 -out ${certfile} -subj /CN=${trusted['certname']}",
+    creates => $certfile,
+    require => Exec['bacula_pki_key'],
+  }
 
   concat { $pki_keypair:
     owner   => 'root',
     group   => $bacula_group,
     mode    => '0640',
-    require => File["${conf_dir}/ssl"],
-  }
-
-  concat::fragment { 'ca':
-    ensure => present,
-    order  => 10,
-    target => $pki_keypair,
-    source => $cafile,
+    require => Exec['bacula_pki_cert'],
   }
 
   concat::fragment { 'certificate':
